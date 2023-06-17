a2d6b26e457a3bf0242d16b9b691e9cd31a2e6fa
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-18 00:16:13 +0300

    update close button

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -10,10 +10,13 @@ type CombinedProps = DrawerProps & Props;
 
 const Drawer: React.FC<CombinedProps> = ({ setCartOpen, children }) => {
   return (
-    <div className="absolute inset-y-0 right-0 w-[500px] bg-white p-4 shadow">
-      <button onClick={() => setCartOpen(false)}>
-        <IoMdCloseCircleOutline className="w-6 h-6" />
-      </button>
+    <div className="absolute inset-y-0 right-0 w-[500px] bg-white p-4 shadow-lg">
+      <div className="flex justify-end">
+        <button onClick={() => setCartOpen(false)}>
+          <IoMdCloseCircleOutline className="w-6 h-6" />
+        </button>
+      </div>
+
       {children}
     </div>
   );
