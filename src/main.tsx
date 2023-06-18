d6d7dfc993e78276cd4558832dd68fef5becb8a7
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-18 22:00:54 +0300

    UPDATE

--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
 
 const client = new QueryClient();
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
-);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <RouterProvider router={router} />
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
